4044cec7bc26c2cd89ceab18d581ef647c83add6
VortexDrops <40763531+VortexDrops@users.noreply.github.com>
2018-07-08 20:55:04 +0200

    Update vortexbot.py

--- vortexbot.py
+++ vortexbot.py
@@ -10,6 +10,11 @@ vortex_server_id = str(os.environ.get("VORTEX_SERVER_ID"))
 welcome_channel_id = str(os.environ.get("WELCOME_CHANNEL_ID"))
 rules_channel_id = str(os.environ.get("RULES_CHANNEL_ID"))
 announcements_channel_id = str(os.environ.get("ANNOUNCEMENTS_CHANNEL_ID"))
+commands_channel_id = str(os.environ.get("COMMANDS_CHANNEL_ID"))
+droplobby_channel_id = str(os.environ.get("DROPLOBBY_CHANNEL_ID"))
+
+status_message_objects = []
+drop_active = False
 
 Client = discord.Client()
 bot = commands.Bot(command_prefix="!")
@@ -32,7 +37,60 @@ async def on_member_join(member):
 	
 @bot.event
 async def on_message(message):
+	
+	global drop_active
+	global status_message_objects
+	
+	droplobby_channel_object = bot.get_server(vortex_server_id).get_channel(droplobby_channel_id)
+	
 	if message.content == "ping":
 		await bot.send_message(message.channel, "pong")
+	
+	
+	status_ask = False
+	for status_command in status_commands:
+		if status_command.lower() in message.content.lower():
+			status_ask = True
+			
+	if message.author.id != bot.user.id:
+		if message.server.id == vortex_server_id:
+			if message.channel.id == commands_channel_id:
+				if (message.content[:11] == "!drop start") or (message.content[:6] == "!start"):
+					if not drop_active:
+						await bot.send_message(droplobby_channel_object, "@here")
+						embed=discord.Embed(title="**Drop lobby started!**", color=0x62f400)
+						embed.add_field(name="To Join: ", value="-Add my SocialClub: **VORTEXDP**", inline=False)
+						status_message_objects.append(await bot.send_message(droplobby_channel_object, embed=embed))
+						
+						drop_active = True
+					else:
+						await bot.send_message(message.channel, "**A drop is already happening.**")
+						
+						
+						
+				elif (message.content[:10] == "!drop stop") or (message.content[:9] == "!drop end") or (message.content[:5] == "!stop"):
+					if drop_active:
+						for status_message_object in status_message_objects:
+							embed=discord.Embed(title="**Drop lobby started!**", color=0x62f400)
+							embed.add_field(name="To Join: ", value="-Add my SocialClub!", inline=False)
+							await bot.edit_message(status_message_object, embed=embed)
+						status_message_objects = []
+						embed=discord.Embed(title="**Drop lobby over!**", description="If you didnt get to join, stay tuned for the next one!", color=0xff060d)
+						await bot.send_message(droplobby_channel_object, " @here")
+						await bot.send_message(droplobby_channel_object, embed=embed)
+						drop_active = False
+					else:
+						await bot.send_message(message.channel, "**There is no current drop happening.**")
+		
+		
+		
+			if (message.content[:7] == "!status") or (message.content[:12] == "!drop status") or (status_ask):
+				if drop_active:
+					embed=discord.Embed(title="**Drop lobby currently active!**", color=0x1cc104)
+					embed.add_field(name="To Join: ", value="-Add my SocialClub: **VORTEXDP**", inline=False)
+					status_message_objects.append(await bot.send_message(message.channel, embed=embed))
+				else:
+					embed=discord.Embed(title="**Drop lobby not active!**", description="Next one coming soon!", color=0xff060d)
+					await bot.send_message(message.channel, embed=embed)
 	
 bot.run(token)
