e1431e9b0eb3d46a66f4c6c0ef09089f230cd41d
VortexDrops <40763531+VortexDrops@users.noreply.github.com>
2018-07-27 20:39:33 +0200

    Update vortexbot.py

--- vortexbot.py
+++ vortexbot.py
@@ -13,6 +13,7 @@ announcements_channel_id = str(os.environ.get("ANNOUNCEMENTS_CHANNEL_ID"))
 commands_channel_id = str(os.environ.get("COMMANDS_CHANNEL_ID"))
 droplobby_channel_id = str(os.environ.get("DROPLOBBY_CHANNEL_ID"))
 logs_channel_id = str(os.environ.get("LOGS_CHANNEL_ID"))
+games_channel_id = str(os.environ.get("GAMES_CHANNEL_ID"))
 
 gmt_plus = 2
 
@@ -25,6 +26,8 @@ status_commands = [
 ]
 status_message_objects = []
 drop_active = False
+
+destruction = False
 
 Client = discord.Client()
 bot = commands.Bot(command_prefix="!")
@@ -54,8 +57,13 @@ async def on_message(message):
 	global drop_active
 	global status_message_objects
 	
+	global destruction
+	global member_hero
+	
+	
 	droplobby_channel_object = bot.get_server(vortex_server_id).get_channel(droplobby_channel_id)
 	logs_channel_object = bot.get_server(vortex_server_id).get_channel(logs_channel_id)
+	games_channel_object = bot.get_server(vortex_server_id).get_channel(games_channel_id)
 	
 	message_time = str(int(str(message.timestamp)[11:13]) + gmt_plus) + str(message.timestamp)[13:19]
 	message_date = "{0}/{1}/{2}".format(str(message.timestamp)[8:10], str(message.timestamp)[5:7], str(message.timestamp)[:4])
@@ -106,6 +114,34 @@ async def on_message(message):
 		
 		
 		
+				elif message.content[:12] == "!destruction":
+					destruction = True
+					starting_spaces = 50
+					for x in range(5):
+						x += 1
+						if x == 1:
+							message_to_edit = await bot.send_message(games_channel_object, ":scream: :house: :house: :house: :house: :house:{0}:bomb::smiling_imp:".format((starting_spaces-10)*" "))
+						else:
+							for y in range(1):
+								await asyncio.sleep(1)
+								if not destruction:
+									break
+							if destruction:
+								await bot.edit_message(message_to_edit, ":scream: :house: :house: :house: :house: :house:{0}:bomb:{1}:smiling_imp:".format((starting_spaces-(x*10))*" ", ((x-1)*10)*" "))
+							else:
+								await bot.edit_message(message_to_edit, ":sweat_smile: :house: :house: :house: :house: :house:{0}:imp:".format((starting_spaces)*" "))
+								await bot.send_message(games_channel_object, "**{0} saved the town! :thumbsup:**".format(member_hero.name))
+								break
+					if destruction:
+						await asyncio.sleep(0.2)
+						await bot.edit_message(message_to_edit, ":fire: :fire: :fire: :fire: :fire: :fire:{0}:smiling_imp:".format(starting_spaces*" "))
+						await asyncio.sleep(0.2)
+						await bot.edit_message(message_to_edit, ":dizzy_face: :house_abandoned: :house_abandoned: :house_abandoned: :house_abandoned: :house_abandoned:{0}:smiling_imp:".format(starting_spaces*" "))
+
+		
+		
+		
+		
 			if (message.content[:7] == "!status") or (message.content[:12] == "!drop status") or (status_ask):
 				if drop_active:
 					embed=discord.Embed(title="**Drop lobby currently active!**", color=0x1cc104)
@@ -126,5 +162,12 @@ async def on_message(message):
 				embed.set_author(name="{0}#{1}".format(message.author.name, message.author.discriminator), icon_url=message.author.avatar_url)
 				embed.set_footer(text="{0} @ {1}".format(message_date, message_time))
 				await bot.send_message(logs_channel_object, embed=embed)
+				
+			
+			
+			elif message.content[:5] == "!save":
+				if destruction:
+					destruction = False
+					member_hero = message.author
 	
 bot.run(token)
